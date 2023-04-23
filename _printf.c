26ab105f96353a47c7479e23d1451b55ac1ab019
mahmoud <mahmoud.m3798@gmail.com>
2023-04-23 15:30:05 -0700

    Task 1

--- _printf.c
+++ _printf.c
@@ -1,15 +1,41 @@
 #include "main.h"
-/**
-* _putchar - function to print character
-* @c: char variable
-* Return: integer
-*/
-int _putchar(char c)
-{
-	return (putchar(c));
-}
+
 /**
 * _printf - function works as printf
+* @format: char pointer
+* @...: pointer
+* Return: integer
+*/
+
+/** 
+* print_char - Helper function to print a single character
+* @c: char variable
+* Returns the number of characters printed (always 1)
+*/
+static int print_char(char c)
+{
+	putchar(c);
+	return (1);
+}
+
+/**
+* print_string - Helper function to print a null-terminated string
+* @s: pointer
+* Returns the number of characters printed
+*/
+static int print_string(char *s)
+{
+	int count = 0;
+
+	while (*s)
+	{
+		putchar(*s++);
+		count++;
+	}
+	return (count);
+}
+/**
+* _printf - function that handles format strings with %c, %s, and %%
 * @format: char pointer
 * @...: pointer
 * Return: integer
@@ -17,50 +43,36 @@ int _putchar(char c)
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int num_chars_printed = 0;
-	char c, *s;
+	int count = 0;
 
 	va_start(args, format);
-	while (*format != '\0')
+	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
 			if (*format == 'c')
 			{
-				c = (char) va_arg(args, int);
-				_putchar(c);
-				num_chars_printed++;
+				char c = (char)va_arg(args, int);
+
+				count += print_char(c);
 			}
 			else if (*format == 's')
 			{
-				*s = va_arg(args, char *);
-				while (*s != '\0')
-				{
-					_putchar(*s);
-					s++;
-					num_chars_printed++;
-				}
+				char *s = va_arg(args, char *);
+
+				count += print_string(s);
 			}
 			else if (*format == '%')
 			{
-				_putchar('%');
-				num_chars_printed++;
-			}
-			else
-			{
-				_putchar(*format);
-				num_chars_printed++;
+				count += print_char('%');
 			}
 		}
 		else
-		{
-			_putchar(*format);
-			num_chars_printed++;
-		}
+			count += print_char(*format);
 		format++;
 	}
 	va_end(args);
-	putchar('\n');
-	return (num_chars_printed);
+	return count;
 }
+
