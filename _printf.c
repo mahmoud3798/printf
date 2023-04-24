89f398a740b46b100cb1cff38dcdbc1a30518b51
mahmoud <mahmoud.m3798@gmail.com>
2023-04-23 17:21:25 -0700

    Task 1

--- _printf.c
+++ _printf.c
@@ -72,6 +72,8 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2]))
+		return (-1);
 	while (*format != '\0')
 	{
 		if (*format == '%')
